a9f92888cdc0db1547f2542651433b0eaf8de4ab
Hunter Cao <yudi.cao@qq.com>
2018-09-26 09:24:30 +0800

    Add simple __name__ related python files.

---
+++ python_project/how_to/file2_keyword__name__.py
@@ -0,0 +1,7 @@
+import file1_keyword__name__
+print "Fie2 __name__ = %s" %__name__
+
+if __name__ == "__main__":
+    print "File2 is bering run directly"
+else:
+    print "File2 is being imported"
