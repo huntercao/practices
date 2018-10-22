6acf76505fe659fe62514a747aeef74424748c62
Hunter Cao <yudi.cao@qq.com>
2018-10-22 20:38:18 +0800

    update main.cpp

--- cpp_primer/c1_5_1/main.cpp
+++ cpp_primer/c1_5_1/main.cpp
@@ -6,9 +6,18 @@ int main()
 {
 	Sales_item book;
 
+	std::cout << "input book: \nisbn, units_sold, price;" << std::endl; 
 	std::cin >> book;
 
+	std::cout << "output: \nisbn, \tunits_sold, \trevenue, \tavg_price;" << std::endl; 
 	std::cout << book << std::endl;
+
+	Sales_item item1, item2;
+	std::cout << "input item1 and item2" << std::endl;
+	std::cin >> item1 >> item2;
+
+	std::cout << "print sum of \"item1 + item2\"" << std::endl;
+	std::cout << item1 + item2 << std::endl;
 
 	return 0;
 }
