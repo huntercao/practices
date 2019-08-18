cc120e71232752e06ad5b28d89fcfc8942dcc8c9
Hunter Cao <yudi.cao@qq.com>
2019-08-18 20:25:59 +0800

    Update ThreadDemo code.

--- java_project/howto/ThreadDemo/src/threaddemo/ThreadDemo.java
+++ java_project/howto/ThreadDemo/src/threaddemo/ThreadDemo.java
@@ -5,17 +5,47 @@
  */
 package threaddemo;
 
+import java.lang.*;
+
 /**
  *
  * @author hunter
  */
-public class ThreadDemo {
+public class ThreadDemo implements Runnable {
 
+    Thread t;
+    
+    ThreadDemo(){
+        
+        t = new Thread(this);
+        System.out.println("Executing " + t.getName());
+        t.start();
+        
+        if(!t.isInterrupted()){
+            t.interrupt();
+        }
+        try {
+            t.join();
+        } catch(InterruptedException e) {}
+    }
+    
+    public void run(){
+        try {
+            while (true){
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.print(t.getName() + " interrupted:");
+            System.out.println(e.toString());
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new ThreadDemo();
+        new ThreadDemo();
     }
     
 }
