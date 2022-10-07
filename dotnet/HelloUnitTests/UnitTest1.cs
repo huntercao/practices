039721c7db958faef476832f2bad5a138f53a71e
Hunter Cao <caohu@vmware.com>
2022-10-07 21:07:23 +0800

    Add sample test case

--- dotnet/HelloUnitTests/UnitTest1.cs
+++ dotnet/HelloUnitTests/UnitTest1.cs
@@ -3,8 +3,19 @@ namespace HelloUnitTests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestPassing()
     {
+        Assert.Equal(4, Add(2, 2));
+    }
 
+    [Fact]
+    public void TestFailing()
+    {
+        Assert.Equal(5, Add(2, 2));
+    }
+
+    int Add(int x, int y)
+    {
+        return x + y;
     }
 }
