feaac3e0ae7861f816dec487c3f496392a035cb4
Hunter Cao <caohu@vmware.com>
2022-10-11 10:33:33 +0800

    Update delegate anonymous function.

--- dotnet/HelloDelegate/HelloDelegate/Program.cs
+++ dotnet/HelloDelegate/HelloDelegate/Program.cs
@@ -1,2 +1,11 @@
 ﻿Func<int, int, int> sum = delegate (int a, int b) { return a + b; };
 Console.WriteLine(sum(3, 4));
+
+Action greet = delegate { Console.WriteLine("Hello!"); };
+greet();
+
+Action<int, double> introduce = delegate { Console.WriteLine("This is world!"); };
+introduce(42, 2.7);
+
+Func<int, int, int> constant = delegate (int _, int _) { return 42; };
+Console.WriteLine(constant(3, 4));
