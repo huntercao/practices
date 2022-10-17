e411d9cb68beb70ecf3ec405e891280fdd6255b7
Hunter Cao <caohu@vmware.com>
2022-10-17 23:18:18 +0800

    Add Hello WireMock

---
+++ dotnet/HelloWireMock.Net/HelloWireMock.Net/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://pmichaels.net/2022/08/06/wiremock-net/
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+var request = Request.Create()
+    .WithPath("/test")
+    .UsingGet();
+
+var response = Response.Create()
+    .WithStatusCode(200)
+    .WithBody("Hello WireMock!");
+
+WireMockServer wireMockServer = WireMockServer.Start(1234);
+wireMockServer
+    .Given(request)
+    .RespondWith(response);
+
+
+HttpClient client = new HttpClient();
+var result = await client.GetAsync("http://localhost:1234/test");
+if (result != null && result.IsSuccessStatusCode)
+{
+   var output = await result.Content.ReadAsStringAsync();
+   Console.WriteLine(output);
+}
